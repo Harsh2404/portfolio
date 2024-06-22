7be91130fd70e567d75ec2fb150f15654c5a643c
Harsh2404 <harshnshah2424@gmail.com>
2024-06-21 22:35:41 -0400

    website format updated, intro and about us joined rearraned sections and fixed margins and padding issue

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,16 +1,36 @@
-import IntroContent from "./IntroContent/IntroContent";
+
 import classes from "./Intro.module.css"
-import ProfilePic from "../../assets/ProfilePic(280x320).jpeg";
+import ProfilePic from "../../assets/home.jpg";
 
 
 const Intro=(props)=>{
     return(
     <div className={classes.Intro}>
-        <IntroContent/>
-        <div className={classes.PhotoContainer}>
+            <div className={classes.IntroContent}>
+                <p className={classes.Hello}>Hello, I'm Harsh Shah</p>
+                <p className={classes.About}>
+                    I'm MEng Computer Engineering graduate from University of Guelph.
+                    Looking for full time opporunity as a Data Analyst or Software Developer.
+                    <br/>
+                    <br/>
+                I'm a passionate, self-proclaimed designer who specializes in full stack development 
+                (React.js & Node.js). I am very enthusiastic about bringing the technical and visual 
+                aspects of digital products to life. User experience, pixel perfect design, and writing 
+                clear, readable, highly performant code matters to me.
+                <br/>
+                <br/>
+                I began my journey as a web developer in 2015, and since then, 
+                I've continued to grow and evolve as a developer, taking on new 
+                challenges and learning the latest technologies along the way. 
+                
+                
+                <br/>
+                <br/>
+                Toronto, CA</p>
+            </div>
+            <div className={classes.PhotoContainer}>
                 <img src={ProfilePic} alt="ProfilePic"/> 
-            
-        </div>
+            </div>
     </div>
     )
 }
